145a013d8666b1a4ff9726c258421bd4c80084f6
Adam Niederer <adam.niederer@gmail.com>
2022-03-17 21:17:05 -0400

    Static link OpenBLAS; add iso build rules

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 CFLAGS=-march=native -O3 -masm=intel -flto -fopenmp -std=gnu2x -ISIMDxorshift/include
 
 manganese: manganese.o tests-512.o tests-256.o tests.o hardware.o SIMDxorshift/simdxorshift128plus.o
-	cc $(CFLAGS) -o manganese manganese.o tests-512.o tests-256.o tests.o hardware.o SIMDxorshift/simdxorshift128plus.o -lcblas
+	cc $(CFLAGS) -o manganese manganese.o tests-512.o tests-256.o tests.o hardware.o \
+	SIMDxorshift/simdxorshift128plus.o OpenBLAS/libopenblas.a \
 
 manganese.o: manganese.c
 	cc $(CFLAGS) -c manganese.c
@@ -21,7 +22,24 @@ hardware.o: hardware.c hardware.h
 SIMDxorshift/simdxorshift128plus.o:
 	 $(MAKE) CFLAGS=-mavx512f -C SIMDxorshift simdxorshift128plus.o
 
+OpenBLAS/libopenblas.a:
+	 $(MAKE) USE_OPENMP=1 USE_THREAD=1 NO_LAPACK=0 DYNAMIC_ARCH=1 DYNAMIC_LIST="HASWELL SKYLAKEX ATOM COOPERLAKE SAPPHIRERAPIDS ZEN" -C OpenBLAS
+
 .PHONY: clean
 clean:
-	rm -f *.o manganese
+	rm -f manganese.iso *.o manganese
+	rm -rf /tmp/manganese-fs /tmp/manganese-iso
+
+.PHONY: clean-all
+clean-all: clean
+	rm -f *.tcz* tinycore.iso
 	$(MAKE) -C SIMDxorshift clean
+	$(MAKE) -C OpenBLAS clean
+
+### Standalone ISO
+
+manganese.iso: tinycore.iso manganese
+	sudo bash build-iso.sh
+
+tinycore.iso:
+	curl -o tinycore.iso "http://tinycorelinux.net/13.x/x86_64/release/CorePure64-13.0.iso"
--- build-iso.sh
+++ build-iso.sh
@@ -1,30 +1,28 @@
-rm -rf iso fs
+rm -rf /tmp/manganese-iso /tmp/manganese-fs
 
-mkdir -p iso fs
+mkdir -p /tmp/manganese-iso/cde/optional /tmp/manganese-fs
 
-xorriso -osirrox on -indev ./CorePure64-13.0.iso -extract / iso
+xorriso -osirrox on -indev tinycore.iso -extract / /tmp/manganese-iso
 
-cd fs
-cat ../iso/boot/corepure64.gz | gunzip | cpio -i -H newc -d
-cd ..
+pushd /tmp/manganese-fs
+cat /tmp/manganese-iso/boot/corepure64.gz | gunzip | cpio -i -H newc -d
+popd
 
 strip manganese
-cp manganese fs/bin
-ln -s lib fs/lib64
-cp /lib/libgomp.so* fs/lib
-cp /lib/libm.so* fs/lib
-cp /lib/libgfortran.so* fs/lib
-cp /lib/libquadmath.so* fs/lib
-cp /lib/libgcc_s.so* fs/lib
-cp /lib/libopenblas.so* fs/lib
-echo "manganese 96%" >> fs/opt/bootlocal.sh
-sed -i -e 's/prompt 1/prompt 0/g' iso/boot/isolinux/isolinux.cfg
+cp manganese /tmp/manganese-fs/bin
+ln -s lib /tmp/manganese-fs/lib64
+mkdir -p /tmp/manganese-fs/home/tc /tmp/manganese-fs/usr/share/licenses/OpenBLAS /tmp/manganese-fs/usr/share/licenses/SIMDxorshift
+mkdir -p
+cp OpenBLAS/LICENSE /tmp/manganese-fs/usr/share/licenses/OpenBLAS
+cp SIMDxorshift/LICENSE /tmp/manganese-fs/usr/share/licenses/SIMDxorshift
+echo -e "\necho -e 'Manganese is also distributed with ABSOLUTELY NO WARRANTY.\nhttps://github.com/AdamNiederer/manganese\n'\necho 'Loading dependencies... (waiting for network to settle)'\nsleep 4\ntce-load -iw gcc_libs\nsudo manganese 98%" >> /tmp/manganese-fs/home/tc/.profile
+sed -i -e 's/prompt 1/prompt 0/g' /tmp/manganese-iso/boot/isolinux/isolinux.cfg
 
-cd fs
-find . | cpio -H newc -o | gzip -9 > ../iso/boot/corepure64.gz
-cd ..
+pushd /tmp/manganese-fs
+find . | cpio -H newc -o | gzip -9 > /tmp/manganese-iso/boot/corepure64.gz
+popd
 
 xorriso -as mkisofs -iso-level 3 -full-iso9660-filenames -volid "manganese" \
         -eltorito-boot boot/isolinux/isolinux.bin -boot-load-size 4 \
         -eltorito-catalog boot/isolinux/boot.cat -boot-info-table \
-        -no-emul-boot -output "manganese.iso" iso
+        -no-emul-boot -output "manganese.iso" /tmp/manganese-iso
